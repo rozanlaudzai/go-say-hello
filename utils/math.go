f8d22b64959c74bfd57c8c6f6b909eda2ded555b
Rozan Laudzai <rozanlaudzai717@gmail.com>
2025-11-09 18:29:29 +0700

    add math.go in utils

---
+++ utils/math.go
@@ -0,0 +1,13 @@
+package utils
+
+func IsPrime(number int) bool {
+	if number < 2 {
+		return false
+	}
+	for i := 2; i*i <= number; i++ {
+		if number%i == 0 {
+			return false
+		}
+	}
+	return true
+}
